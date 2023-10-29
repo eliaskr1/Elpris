e7cdbee7b377e390d74b926954c2d7a2bc3ef750
Elias Kroon <elias.kroon19@gmail.com>
2023-10-29 14:39:09 +0100

    added some error handling

--- application/app.py
+++ application/app.py
@@ -1,6 +1,7 @@
+from urllib.error import HTTPError
 from flask import Flask, render_template, request
 from application import func
-from datetime import datetime, timedelta, date
+
 
 app = Flask(__name__)
 
@@ -12,17 +13,31 @@ def index():
 @app.route("/form")
 def form():
     '''Funktion för endpointen "form"'''
-    
-    return render_template("form.html", max_date=func.get_max_date())
+    max_date=func.get_max_date()
+    return render_template("form.html", max_date=max_date)
 
 @app.route("/api", methods=["POST"])
 def api_post():
     '''Funktion som callas när man fyllt formulär på "form" sidan'''
-    date = request.form["date"]
-    year, month, day = date.split('-')
-    prisklass = request.form["prisklass"]
-    api_url = f"https://www.elprisetjustnu.se/api/v1/prices/{year}/{month}-{day}_{prisklass}.json"
+    try:
+        date = request.form["date"]
+        year, month, day = date.split('-')
+        prisklass = request.form["prisklass"]
+        api_url = f"https://www.elprisetjustnu.se/api/v1/prices/{year}/{month}-{day}_{prisklass}.json"
 
-    data = func.json_data_to_html_table(api_url)
+        data = func.json_data_to_html_table(api_url)
 
-    return render_template("table.html", data=data)
+        return render_template("table.html", data=data)
+    except ValueError as ve:
+        # Felhantering för tomt datum fält i "/form" endpoint. 
+        return "Vänligen ange giltigt datum."
+
+@app.route("/api", methods=["GET"])
+def api_get():
+    '''Funktion som callas om man försöker komma in på 
+    "/api" endpoint med "GET" metoden'''
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("index.html"), 404
